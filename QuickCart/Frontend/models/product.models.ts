7482e3cacf90a0a8e14865949c0ceb28b2b26eb2
SakshiN123456 <sdnalawade10@gmail.com>
2025-09-27 19:09:01 +0530

    Create product.models.ts

---
+++ QuickCart/Frontend/models/product.models.ts
@@ -0,0 +1,18 @@
+export interface Product {
+  productId: number;
+  name: string;
+  price: number;
+  imageurl: string;
+}
+
+export interface CartItem   {
+  productId: number;
+  quantity: number;
+
+}
+
+
+
+
+
+
